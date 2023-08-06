69e715c153a53414212387d3c29451009ed6bf57
Aman Shinde <1703amanshinde98@gmail.com>
2023-08-06 21:16:11 +0530

    Add backend API

--- api/v1/products.js
+++ api/v1/products.js
@@ -1,17 +1,83 @@
 const Product = require("../../model/product");
 
-module.exports.getProducts = (req, res) => {
-  // TODO GET all api
+module.exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    const productList = [];
+    for (let prod of products) {
+      productList.push({
+        id: prod._id,
+        name: prod.name,
+        quantity: prod.quantity,
+      });
+    }
+    return res.json({ data: { products: [...productList] } });
+  } catch (err) {
+    return res.status(500).json({
+      error: {
+        message: "Internal server error",
+        code: err,
+      },
+    });
+  }
 };
 
 module.exports.create = async (req, res) => {
-  // TODO Add create API logic
+  try {
+    const product = await Product.create(req.body.product);
+    return res.json({
+      data: { product: { name: product.name, quantity: product.quantity } },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: {
+        message: "Internal server error",
+        code: err,
+      },
+    });
+  }
 };
 
 module.exports.update = async (req, res) => {
-  // TODO Update product
+  try {
+    const number = req.query.number;
+    const product = await Product.findById(req.params.id);
+    product.quantity = number;
+    const updatedProduct = await product.save();
+    return res.json({
+      data: {
+        product: {
+          id: updatedProduct._id,
+          name: updatedProduct.name,
+          quantity: updatedProduct.quantity,
+        },
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: {
+        message: "Internal server error",
+        code: err,
+      },
+    });
+  }
 };
 
 module.exports.delete = async (req, res) => {
-  // TODO Delete product
+  try {
+    const product = await Product.findById(req.params.id);
+    const resp = await product.deleteOne();
+    res.json({
+      data: {
+        message: "product deleted",
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: {
+        message: "Internal server error",
+        code: err,
+      },
+    });
+  }
 };
